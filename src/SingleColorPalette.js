48c3a2c958e415acb7f61372bcbe53e4b0d041e5
Ryan O'Hare <ryan.1.ohare@gmail.com>
2019-11-07 11:05:24 +0000

    Add SingleColorPalette and Link

---
+++ src/SingleColorPalette.js
@@ -0,0 +1,10 @@
+ import React, { Component } from 'react';
+
+ class SingleColorPalette extends Component{
+   render(){
+     return(
+        <h1>SingleColorPalette</h1>
+     );
+   }
+ }
+ export default SingleColorPalette;
