4ce66ee711c4283ab6eb16a0c9ec7a6c6a0118f8
Ryan O'Hare <ryan.1.ohare@gmail.com>
2019-11-11 09:48:03 +0000

    Add new Palette Form and link

---
+++ src/NewPaletteForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NewPaletteForm extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h1>New Palette Form</h1>
+      </div>
+    );
+  }
+}
+
+export default NewPaletteForm;
