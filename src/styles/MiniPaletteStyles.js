c3c43a183b97f6538f7d070019a4af93a84c0a58
Ryan O'Hare <ryan.1.ohare@gmail.com>
2019-11-15 21:29:38 +0000

    Add Delete Icon to MiniPalette

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -4,10 +4,11 @@ export default {
     border: '1px solid black',
     borderRadius: '5px',
     padding: '0.5rem',
-    postion: 'relative',
+    position: 'relative',
     overflow: 'hidden',
-    '&:hover':{
-      cursor : 'pointer'
+    cursor : 'pointer',
+    '&:hover svg':{
+      opacity: 1
     }
   },
   colors: {
@@ -38,5 +39,20 @@ export default {
     margin: '0 auto',
     position: 'relative',
     marginBottom: '-3.5px'
+  },
+  delete: {
+
+  },
+  deleteIcon: {
+    color: 'white',
+    backgroundColor: '#eb3d30',
+    width: '20px',
+    height: '20px',
+    position: 'absolute',
+    right: '0px',
+    top: '0px',
+    padding: '10px',
+    zIndex: 10,
+    opacity: 0
   }  
 }
