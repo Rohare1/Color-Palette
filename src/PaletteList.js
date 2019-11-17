6a38f46c3d854a582c3d44407a885463616ddacf
Ryan O'Hare <ryan.1.ohare@gmail.com>
2019-11-17 16:43:22 +0000

    Refactor MiniPalette with PureComponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -26,6 +26,7 @@ class PaletteList extends Component{
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
   openDialog(id){
     this.setState({openDeleteDialog: true, deletingId: id});
@@ -33,11 +34,9 @@ class PaletteList extends Component{
   closeDialog(){
     this.setState({openDeleteDialog: false, deletingId: ''});
   }
-
   goToPalette(id){
-    this.props.history.push(`/palette/${id}`)
+    this.props.history.push(`/palette/${id}`);
   }
-
   handleDelete(){
     this.props.deletePalette(this.state.deletingId);
     this.closeDialog(); 
@@ -57,7 +56,7 @@ class PaletteList extends Component{
               <CSSTransition key={palette.id} classNames='fade' timeout={500}>
               <MiniPalette 
                 {...palette} 
-                handleClick={() => this.goToPalette(palette.id)}
+                goToPalette={this.goToPalette}
                 // handleDelete={deletePalette}
                 openDialog={this.openDialog}
                 key={palette.id}
