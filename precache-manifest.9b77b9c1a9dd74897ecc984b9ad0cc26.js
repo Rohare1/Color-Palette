f21a7c1f182a623e7b49c9a5ebb43480a0dfe9bd
Ryan o'Hare <ryan.1.ohare@gmail.com>
2019-11-20 19:39:30 +0000

    Updates

---
+++ precache-manifest.9b77b9c1a9dd74897ecc984b9ad0cc26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444c8b986acfa896d6a753724c2b167d",
+    "url": "/colors-app/index.html"
+  },
+  {
+    "revision": "cec6e890bc767ba97644",
+    "url": "/colors-app/static/css/2.5774dd83.chunk.css"
+  },
+  {
+    "revision": "8d724d3dc667fd4698d7",
+    "url": "/colors-app/static/css/main.224ac33f.chunk.css"
+  },
+  {
+    "revision": "cec6e890bc767ba97644",
+    "url": "/colors-app/static/js/2.d67da587.chunk.js"
+  },
+  {
+    "revision": "8d724d3dc667fd4698d7",
+    "url": "/colors-app/static/js/main.d808b72f.chunk.js"
+  },
+  {
+    "revision": "1020afd3eea0412a44b7",
+    "url": "/colors-app/static/js/runtime-main.ffe96785.js"
+  },
+  {
+    "revision": "b004491070b902a916ac7925780146bb",
+    "url": "/colors-app/static/media/bg.b0044910.svg"
+  }
+]);
